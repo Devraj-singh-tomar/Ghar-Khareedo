b815694c2134e2f702f0e1a9429a914230dad281
devraj singh tomar <devrajsingh.t11@gmail.com>
2024-04-29 16:46:48 +0530

    complete delete user listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -9,4 +9,23 @@ export const createListing = async (req, res, next) => {
         next(error);
     }
 
+};
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(404, 'listing not found')
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'you can only delete your own listing'))
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('listing has been deleted')
+    } catch (error) {
+        next(error);
+    }
 }
