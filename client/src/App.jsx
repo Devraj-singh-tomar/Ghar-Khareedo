3e88a6e6b4372cfdc9ae0d2bcc1d66c790de5a30
devraj singh tomar <devrajsingh.t11@gmail.com>
2024-05-03 19:11:31 +0530

    create search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PersonalRoute from "./components/PersonalRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from './pages/Listing'
+import Search from "./pages/Search";
 
 
 export default function App() {
@@ -24,6 +25,8 @@ export default function App() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
         <Route path="/listing/:listingId" element={<Listing />}></Route>
+        <Route path="/search" element={<Search/>}></Route>
+
         <Route element={<PersonalRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
